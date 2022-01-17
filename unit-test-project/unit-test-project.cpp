751e2117107717d1d4070d57048f6707d062a559
sp0nsor <spons0rr1703@gmail.com>
2022-01-18 01:50:31 +0300

    Создан проект-заготовка для модульных тестов

---
+++ unit-test-project/unit-test-project.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestproject
+{
+	TEST_CLASS(unittestproject)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::Fail();
+		}
+	};
+}
