db48724568a3d73967b9377a0ba8db3018e0705f
sp0nsor <spons0rr1703@gmail.com>
2022-01-18 00:36:10 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #5. Internet protocol\n";
     cout << "Author: Roman Kokotkin\n";
+    cout << "Group: 14\n";
     return 0;
 }
