4ae762273cdf123a55d3889cb5d2c23ff630d9d3
sp0nsor <spons0rr1703@gmail.com>
2022-01-18 01:11:17 +0300

    Добавлена фильтрация вывода

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #include "protocol.h"
 #include "file_reader.h"
 #include "constants.h"
+#include "filter.h"
 
 int main()
 {
@@ -47,6 +48,64 @@ int main()
             cout << protocols[i]->path << endl;
             cout << '\n';
         }
+
+        bool (*check_function)(protocol*) = NULL; // check_function - это указатель на функцию, возвращающую значение типа bool,
+                                                          // и принимающую в качестве параметра значение типа book_subscription*
+        cout << "\nВыберите способ фильтрации или обработки данных:\n";
+        cout << "1) Протокол использования сети Интернет программой Skype.\n";
+        cout << "2) Протокол использования сети Интернет после 8:00:00.\n";
+        cout << "\nВведите номер выбранного пункта: ";
+        int item;
+        cin >> item;
+        cout << '\n';
+        switch (item)
+        {
+        case 1:
+            check_function = check_results_by_skype; // присваиваем в указатель на функцию соответствующую функцию
+            cout << "***** Протокол использования сети Интернет программой Skype *****\n\n";
+            break;
+        case 2:
+            check_function = check_results_by_time; // присваиваем в указатель на функцию соответствующую функцию
+            cout << "***** Протокол использования сети Интернет после 8:00:00 *****\n\n";
+            break;
+        default:
+            throw "Некорректный номер пункта";
+        }
+        if (check_function)
+        {
+            int new_size;
+            protocol** filtered = filter(protocols, size, check_function, new_size);
+            for (int i = 0; i < new_size; i++)
+            {
+                /********** вывод времени использования **********/
+                cout << "Начало..........: ";
+                // вывод времени начала сессии использования
+                cout << setw(2) << setfill('0') << filtered[i]->start.hour << ":";
+                cout << setw(2) << setfill('0') << filtered[i]->start.min << ":";
+                cout << setw(2) << setfill('0') << filtered[i]->start.sec << endl;
+                cout << "Конец...........: ";
+                // вывод времени конца сессии использования
+                cout << setw(2) << setfill('0') << filtered[i]->end.hour << ":";
+                cout << setw(2) << setfill('0') << filtered[i]->end.min << ":";
+                cout << setw(2) << setfill('0') << filtered[i]->end.sec << endl;
+
+                /********** вывод передачи данных **********/
+                cout << "Получено........: ";
+                // вывод размера полученных данных в байтах
+                cout << filtered[i]->received << endl;
+                cout << "Отправлено......: ";
+                // вывод размера отправленных данных в байтах
+                cout << filtered[i]->sent << endl;
+
+                /********** вывод программы **********/
+                // вывод полного пути к исполняемой программе
+                cout << "Программа.......: ";
+                cout << filtered[i]->path << endl;
+                cout << '\n';
+            }
+            delete[] filtered;
+        }
+
         for (int i = 0; i < size; i++)
         {
             delete protocols[i];
